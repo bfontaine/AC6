4b638ffe9d3253719ab0a39b9953cb8d35c4a8b6
david <galichet.david@gmail.com>
2013-05-21 14:28:17 +0200

    add function check_vdef and check_exp with is Not implemented

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -1,8 +1,6 @@
 open Ast
 open Runtime
 
-(* type env = ?  ?*)
-
 let flag = ref false
 
 (**
@@ -30,7 +28,26 @@ let program p =
              * rest of the program *)
             let e' = begin match d with
             | DType(_) -> failwith "DType Not implemented"   
-            | DVal(_)  -> failwith "DVal  Not implemented"
+            | DVal(v)  -> check_vdef v e
             end in check_type defs e'
+
+    and check_vdef x env =
+        match x match 
+        | Simple(_, exp )       -> failwith "Simple Not implemented" 
+        | MutuallyRecursive(_)  -> failwith "MutuallyRecursive Not implemented"
+
+    and check_exp exp env = 
+        match exp with
+        | EChar(_)      -> failwith "EChar ot implemented" 
+        | EInt(_)       -> failwith "EInt ot implemented" 
+        | EString(_)    -> failwith "EString ot implemented" 
+        | EAnnot(_,_)   -> failwith "EAnnot ot implemented" 
+        | EApp(_,_)     -> failwith "EApp ot implemented" 
+        | ESum(_,_,_)   -> failwith "ESum ot implemented" 
+        | EProd(_,_)    -> failwith "EProd ot implemented" 
+        | ECase(_,_)    -> failwith "ECase ot implemented" 
+        | EFun(_,_)     -> failwith "EFun ot implemented" 
+        | EDef(_,_)     -> failwith "EDef ot implemented" 
+        | ESeq(_)       -> failwith "ESeq ot implemented" 
     in
       check_type p (e) 
