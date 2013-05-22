43c9abcbac07ff1809f49440b52555062dc168cf
david <galichet.david@gmail.com>
2013-05-22 11:46:09 +0200

    created type env and value_type

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -1,4 +1,18 @@
+open AST
+open Operator
+
 let flag = ref false
+
+let env = (value_identifier * value_type option ref)list
+
+let value_type = 
+    | TInt 
+    | TChar
+    | TString
+    | TBool
+    | TCustom value_type
+    | TFun value_type * value_type
+
 
 let program p = 
   if !flag then 
