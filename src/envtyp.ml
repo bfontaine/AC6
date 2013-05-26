6351ba0aad080a0b5f4b2333e5401d51f57a2889
Baptiste Fontaine <batifon@yahoo.fr>
2013-05-26 19:27:16 +0200

    Comments & formatting.

--- src/envtyp.ml
+++ src/envtyp.ml
@@ -1,46 +1,39 @@
 open AST
 
-
-(** Exception of check typing **)
+(* Type-checking exceptions *)
 exception UndeclaredVariable of value_identifier
-exception SimpleErrorTyping
-exception EFunErrorTyping
-exception EAnnotErrorTyping
-exception EAppErrorTyping
+exception EFunTypingError
+exception EAppTypingError
 exception EAppErrorTVar of string * int
-exception EVarErrorTyping
-exception PSumErrorTyping
-exception DTypeErrorSig
-exception TVarErrorTyping 
+exception PSumTypingError
+exception TVarTypingError 
 exception UnificationError
-exception UnificationError_TS_TP
-exception BranchsErrorVide
-exception BranchErrorUnion
+exception TSumTProdUnificationError
+exception EmptyBranchError
 
-(** Environment of typing  **)
-type env = (value_identifier * AST.typ option ref)list
+(* Environment of typing *)
+type env = (value_identifier * AST.typ option ref) list
 
-(** Signature of typping**)
-let sign = Hashtbl.create 42
+(* Types signatures *)
+let sign = Hashtbl.create 64
 
-let add_op_defauld () =
-    Hashtbl.add sign (TIdentifier("int",-1))  ();
-    Hashtbl.add sign (TIdentifier("char",-1))  ();
-    Hashtbl.add sign (TIdentifier("string",-1))  ();
-    Hashtbl.add sign (TIdentifier("bool",-1))  ();
-    Hashtbl.add sign (TIdentifier("U",-1))  ()
+let add_op_default () =
+  Hashtbl.add sign (TIdentifier("int",    -1 )) ();
+  Hashtbl.add sign (TIdentifier("char",   -1 )) ();
+  Hashtbl.add sign (TIdentifier("string", -1 )) ();
+  Hashtbl.add sign (TIdentifier("bool",   -1 )) ();
+  Hashtbl.add sign (TIdentifier("U",      -1 )) ()
 
-
-(** Abstract variable counter*)
+(* Abstract types counter *)
 let compteur = ref (-1)
 
+(* Increment the abstract types counter and return the new value. *)
 let compt () = incr compteur ; !compteur
 
-(** Function on environment**)
 let empty () = []
 
 let bind x v env = 
-    match x with 
+  match x with 
     | Unnamed -> env
     | Named x -> (x, ref (Some v)) :: env
 
@@ -53,6 +46,7 @@ let identifier p =
   let p = AST.EVar p in
   Operator.is_binop p || Operator.is_unop p
 
+(* helpers *)
 let ( --> ) x y = 
   match x with 
   | AST.EVar x -> (x, y)
@@ -61,61 +55,69 @@ let ( --> ) x y =
 let typage x y = TVar(TIdentifier(x,y), [])
 let (--->) (x,z) (y,w) = TArrow( typage x z ,typage y w )
 let (&-->) (x,z) y = TArrow(typage x z ,y )
- 
+
 let lookup x = List.assoc x [
- Operator.minus       --> (("int",-1)   &--> (("int",-1) ---> ("int",-1)));
- Operator.plus        --> (("int",-1)   &--> (("int",-1) ---> ("int",-1)));
- Operator.star        --> (("int",-1)   &--> (("int",-1) ---> ("int",-1)));
- Operator.slash       --> (("int",-1)   &--> (("int",-1) ---> ("int",-1)));
- Operator.percent     --> (("int",-1)   &--> (("int",-1) ---> ("int",-1)));
- Operator.eq          --> (("_alpha_",compt ())  &--> (("_alpha_",compt ()) ---> ("bool",-1)));
- Operator.bangeq      --> (("_alpha_",compt ())  &--> (("_alpha_",compt ()) ---> ("bool",-1)));
- Operator.andand      --> (("bool",-1)  &--> (("bool",-1) --->("bool",-1)));
- Operator.pipepipe    --> (("bool",-1)  &--> (("bool",-1) --->("bool",-1)));
- Operator.le          --> (("int",-1)  &--> (("int",-1) ---> ("bool",-1)));
- Operator.ge          --> (("int",-1)  &--> (("int",-1) ---> ("bool",-1)));
- Operator.lt          --> (("int",-1)  &--> (("int",-1) ---> ("bool",-1)));
- Operator.gt          --> (("int",-1)  &--> (("int",-1) ---> ("bool",-1)));
- Operator.boolean_not --> (("bool", -1) ---> ("bool",-1));
- Operator.negate      --> (("int" , -1) ---> ("int",-1));
+  Operator.minus        --> (("int", -1)  &--> (("int", -1) ---> ("int", -1)));
+  Operator.plus         --> (("int", -1)  &--> (("int", -1) ---> ("int", -1)));
+  Operator.star         --> (("int", -1)  &--> (("int", -1) ---> ("int", -1)));
+  Operator.slash        --> (("int", -1)  &--> (("int", -1) ---> ("int", -1)));
+  Operator.percent      --> (("int", -1)  &--> (("int", -1) ---> ("int", -1)));
+  Operator.eq           -->
+    (("_alpha_", compt ()) &--> (("_alpha_", compt ()) ---> ("bool", -1)));
+  Operator.bangeq       -->
+    (("_alpha_", compt ()) &--> (("_alpha_", compt ()) ---> ("bool", -1)));
+  Operator.andand       --> (("bool", -1) &--> (("bool", -1) --->("bool", -1)));
+  Operator.pipepipe     --> (("bool", -1) &--> (("bool", -1) --->("bool", -1)));
+  Operator.le           --> (("int", -1)  &--> (("int", -1) ---> ("bool",  -1)));
+  Operator.ge           --> (("int", -1)  &--> (("int", -1) ---> ("bool",  -1)));
+  Operator.lt           --> (("int", -1)  &--> (("int", -1) ---> ("bool",  -1)));
+  Operator.gt           --> (("int", -1)  &--> (("int", -1) ---> ("bool",  -1)));
+  Operator.boolean_not  --> (("bool", -1) ---> ("bool", -1));
+  Operator.negate       --> (("int" , -1) ---> ("int",  -1));
 ]
 
 let rec lookup_ref x env = 
-    match env with
-    | [] -> None
-    | (x',t)::env' -> 
-        if x = x' 
-            then !t 
-        else lookup_ref x env'
+  match env with
+  | [] -> None
+  | (x',t)::env' -> 
+      if x = x' 
+      then !t 
+      else lookup_ref x env'
 
 let rec define_ref x t env =
-    match env with
+  match env with
     | [] -> ()
     | (x', t')::env' ->
-        if x' = x then t' := (Some t )
+        if x' = x
+        then t' := (Some t )
         else define_ref x t env'
 
-let tInt = TVar(TIdentifier("int",-1),[])
-let tChar = TVar(TIdentifier("char",-1),[])
-let tString = TVar(TIdentifier("string",-1),[])
-let tBool = TVar(TIdentifier("bool",-1),[])
-let tUnit = TVar(TIdentifier("U",-1),[])
+let tInt    = TVar(TIdentifier("int",    -1), [])
+let tChar   = TVar(TIdentifier("char",   -1), [])
+let tString = TVar(TIdentifier("string", -1), [])
+let tBool   = TVar(TIdentifier("bool",   -1), [])
+let tUnit   = TVar(TIdentifier("U",      -1), [])
 
-let list_compare l_1 l_2 ty = 
-    if l_1 = l_2 then ty 
-    else raise UnificationError_TS_TP
+(* Compares two TSum/TProd lists, and return their type *)
+let list_compare l1 l2 ty = 
+  if l1 = l2 then ty 
+  else raise TSumTProdUnificationError
 
-let rec unification ty ty_exp =
-    match ty, ty_exp with
-    | ty , ty_exp when ty = ty_exp -> ty
-    | TVar(tI,_), TVar(tI_ex,_) when tI = tI_ex-> ty 
-    | TVar(_,_), TVar(TIdentifier(s,nb),_) when (nb >= 0) -> ty 
-    | TVar(TIdentifier(s,nb),_), TVar(_,_) when (nb >= 0) -> ty_exp 
-    | TRec(_,_), TRec(_,_)      -> failwith "Unif TRec Not Implemented"
-    | TArrow(a1,b1), TArrow(a2,b2)  -> TArrow(unification a1 a2, unification b1 b2)
-    | TSum(l_CI_1), TSum(l_CI_2) -> 
-           list_compare (List.sort compare l_CI_1)  (List.sort compare l_CI_2) ty
-    | TProd(l_CI_1), TProd(l_CI_2)  ->
-           list_compare (List.sort compare l_CI_1)  (List.sort compare l_CI_2) ty
-    |(_,_)                      -> raise UnificationError
+(* Unifies two types *)
+let rec unification t1 t2 =
+  match t1, t2 with
+  | t1 , t2 when t1 = t2 -> t1
+  | TVar(tI, _), TVar(tI_ex, _) when tI = tI_ex           -> t1
+  | TVar(_, _), TVar(TIdentifier(s,nb), _) when (nb >= 0) -> t1
+  | TVar(TIdentifier(s, nb), _), TVar(_, _) when (nb >= 0) -> t2
+  | TArrow(a1, b1), TArrow(a2, b2) ->
+      TArrow(unification a1 a2, unification b1 b2)
+  | TSum(l1), TSum(l2) ->
+      list_compare (List.sort compare l1)  (List.sort compare l2) t1
+  | TProd(l1), TProd(l2) ->
+      list_compare (List.sort compare l1)  (List.sort compare l2) t1
+  | TRec(_, _), TRec(_, _) ->
+      failwith "Unif TRec Not Implemented"
+  | (_, _) ->
+      raise UnificationError
 
--- src/typecheck.ml
+++ src/typecheck.ml
@@ -9,265 +9,283 @@ let flag = ref false
  * @param p the program (list of definitions)
  * @return an unit
  **)
-let program p = 
-  
- (**
-  * Check a program with an environment of typing.
+let program p =
+
+  (**
+   * Check a program with an environment of typing.
   *
   * @param p the program (list of definitions)
   * @param e the environment
   * @return the new environment
   **)
- let rec check p e =
-      match p with
+  let rec check prog envt =
+    match prog with
       (* No definitions *)
       | [] -> ()
       (* One or more definitions *)
-      | d::defs -> 
-        (* Check the definition, and iter on the
-         * rest of the program *)
-        let e' = begin match d with
-          | DType(ty_id,ty_ids,ty)-> check_dtype ty_id ty_ids ty e
-          | DVal(v) -> check_vdef v e 
-        end in check defs e'
+      | d::defs ->
+          (* Check the definition, and iter on the
+           * rest of the program *)
+          let envt' = begin match d with
+            | DType(type_id, type_ids, ty) ->
+                check_dtype type_id type_ids ty envt
+            | DVal(v) ->
+                check_vdef v envt
+          end in check defs envt'
 
-  and check_dtype ty_id ids ty e =
-    failwith "DType Not implemented" 
 
-  and check_vdef v e =
-    match v with
-    | Simple(Binding(i, ty), ex) -> check_simple i ty ex e 
-    | MutuallyRecursive(l) -> check_mutually_recursive l e
+  (* Evaluates and verifies a type declaration. *)
+  and check_dtype type_id type_ids ty envt =
+    failwith "DType Not implemented"
 
-  and check_expr exp e pr_ex =
-	 match exp with
-     | EInt(_)	    -> tInt 
-     | EChar(_)     -> tChar
-     | EString(_)   -> tString
-     | EVar(v)	    -> check_EVar v e pr_ex
-     | EAnnot(ex,ty)	->
-        let t_ty = check_typ ty e in
-        let t_ex = check_expr ex e (Some t_ty) in
-        unification t_ty t_ex
 
-     | ESeq(es)     -> check_ESeq es e pr_ex 
-     
-     | EDef(v,exp2)    ->
-        check_expr exp2 (check_vdef v e) pr_ex
-     
-     | EFun(Binding(i,ty),exp)    -> check_EFun i ty exp e pr_ex
+  (* Check the types of a VDefinition *)
+  and check_vdef vdef envt =
+    match vdef with
+    | Simple(Binding(i, ty), ex) -> check_simple i ty ex envt
+    | MutuallyRecursive(l)       -> check_mutually_recursive l envt
 
-     | EApp(f,e1)   -> check_EApp f e1 e pr_ex
-    
-     | ECase(ty,brs)	->
+  (* Check the type of an expression given a predicted type, for example
+     in ( E : int ), the type of E is "predicted" to be 'int'. *)
+  and check_expr exp envt predicted_type =
+    match exp with
+     | EInt _         -> tInt
+     | EChar _        -> tChar
+     | EString _      -> tString
+     | EVar(v)        -> check_EVar v envt predicted_type
+     | EAnnot(ex, ty) ->
+         let t_ty = check_typ ty envt in
+           let t_ex = check_expr ex envt (Some t_ty) in
+             unification t_ty t_ex
+
+     | ESeq(es)       -> check_ESeq es envt predicted_type
+
+     | EDef(vdef, exp2)  ->
+         check_expr exp2 (check_vdef vdef envt) predicted_type
+
+     | EFun(Binding(i, ty), exp) -> check_EFun i ty exp envt predicted_type
+
+     | EApp(f, e)     -> check_EApp f e envt predicted_type
+
+     | ECase(ty, branchs) ->
         begin match ty with
-        | None   -> check_branchs brs e
+        | None   -> check_branchs branchs envt
         | Some p ->
-            let t_ty = check_typ p e in
-            let t_brs = check_branchs brs e in
-            unification t_ty t_brs
+            let t_ty = check_typ p envt in
+              let t_branchs = check_branchs branchs envt in
+                unification t_ty t_branchs
         end
 
-     | ESum(constr,ty_op,ex_op)	-> TSum([check_ESum constr ty_op ex_op e])
-    
-     | EProd(ty_op,l_pr)	-> TProd(check_EProd l_pr ty_op e)
+      | ESum(constr, ty_op, ex_op) ->
+          TSum([check_ESum constr ty_op ex_op envt])
+
+      | EProd(ty_op, l_pr) -> TProd(check_EProd l_pr ty_op envt)
+
 
   and check_mutually_recursive l e =
-    let e' = 
-        (* 1- declare empty functions *)
-        List.fold_left (fun e' -> function
-        | (Binding(i, _), _) -> bind i (typage "_alpha_" (compt ())) e')
-        e l
-    in
-    (* 2- bind their bodies *)
-    List.iter (function
-      | (Binding(Named(i),_), body) -> 
-            let t_body = check_expr body e' None  in
+    let e' =
+      (* 1- declare empty functions *)
+      List.fold_left (fun e' -> function
+          (Binding(i, _), _) -> bind i (typage "_alpha_" (compt ())) e') e l
+        in
+        (* 2- bind their bodies' types *)
+        List.iter (function
+          (Binding(Named(i), _), body) ->
+            let t_body = check_expr body e' None in
             define_ref i t_body e'
-      | _ -> ()
-    ) l; e'
+        | _ -> ()
+        ) l; e'
 
 
+  (* Check the type of a simple definition (val x = ...) *)
   and check_simple i ty ex e =
     let ty_ex = check_expr ex e None in
-    match ty with 
-     | None   -> bind i ty_ex e
-     | Some p -> let ty' = check_typ p e in
-        let ty_fi = unification ty' ty_ex in 
-            bind i ty_fi e
+      match ty with
+      | None   -> bind i ty_ex e
+      | Some p -> let ty' = check_typ p e in
+        let ty_fi = unification ty' ty_ex in
+          bind i ty_fi e
 
-  and check_ESeq es e  pr_ex =
-      match es with
-        | [] -> tUnit
-        | [ex] ->
-          check_expr ex e pr_ex
-        | ex::es' ->
-          let _ = check_expr ex e pr_ex in
+
+  (* Check the type of a ESeq *)
+  and check_ESeq es e pr_ex =
+    match es with
+       | [] -> tUnit
+       | [ex] ->
+           check_expr ex e pr_ex
+       | ex::es' ->
+           let _ = check_expr ex e pr_ex in
              check_ESeq es' e pr_ex
 
   and check_EApp f e1 e pr_ex =
-       let t_f = check_expr f e None in
-        match t_f with
-        | TArrow(ta_1,ta_2 )  -> 
-            let tA = check_TArrow ta_1 ta_2 e1 e in
+    let t_f = check_expr f e None in
+      match t_f with
+        | TArrow(ta_1, ta_2 )  ->
+          let tA = check_TArrow ta_1 ta_2 e1 e in
             begin match pr_ex with
             | None -> tA
             | Some p -> unification tA p
             end
-        | TVar(TIdentifier("_alpha_",nb),_) when nb >= 0 -> check_TVar_alpha f t_f e1 e pr_ex 
-        | TVar(TIdentifier(str,nb),_) ->  raise (EAppErrorTVar (str, nb))
+        | TVar(TIdentifier("_alpha_", nb), _) when nb >= 0 ->
+            check_TVar_alpha f t_f e1 e pr_ex
+        | TVar(TIdentifier(str, nb), _) ->  raise (EAppErrorTVar (str, nb))
         | TSum(_) -> failwith "EApp -> TSum Not implemented"
         | TProd(_) -> failwith "EApp -> TProd Not implemented"
-        | TRec(_,_) -> failwith "EApp -> TRec Not implemented"
+        | TRec(_, _) -> failwith "EApp -> TRec Not implemented"
 
   and check_TArrow ta_1 ta_2 e1 e =
-        let t_ex = check_expr e1 e (Some ta_1) in
-        match (ta_1,t_ex) with
-        | (t1,t2) when t1 = t2 -> ta_2
-        | (TVar(TIdentifier("_alpha_",nb),_),t1) when nb >= 0 -> ta_2
-        | (t1,TVar(TIdentifier("_alpha_",nb),_)) when nb >= 0 -> ta_2
-        | (_, _) -> raise EAppErrorTyping 
+    let t_ex = check_expr e1 e (Some ta_1) in
+      match (ta_1, t_ex) with
+      | (t1, t2) when t1 = t2 -> ta_2
+      | (TVar(TIdentifier("_alpha_", nb), _), t1) when nb >= 0 -> ta_2
+      | (t1, TVar(TIdentifier("_alpha_", nb), _)) when nb >= 0 -> ta_2
+      | (_, _) -> raise EAppTypingError
 
   and check_TVar_alpha f t_f e1 e pr_ex =
-          let ty_f = match pr_ex with
-          | Some p -> p
-          | None -> typage "_alpha_" (compt ())
-          in
-          match f with 
-          |EVar(v) -> define_ref v (TArrow(t_f, ty_f)) e ;check_TArrow t_f ty_f e1 e            
-          | _ -> failwith "EApp -> TVar function Error "
+    let ty_f = match pr_ex with
+      | Some p -> p
+      | None -> typage "_alpha_" (compt ())
+    in
+      match f with
+      |EVar(v) ->
+          define_ref v (TArrow(t_f, ty_f)) e ;
+          check_TArrow t_f ty_f e1 e
+      | _ -> failwith "EApp -> TVar function Error "
 
   and check_EFun i ty exp e pr_ex =
     match ty with
-    | None    ->
-        let ty'' = typage "_alpha_"  (compt ()) in
+    | None ->
+      let ty'' = typage "_alpha_"  (compt ()) in
         let e' = bind i ty'' e in
-        let t_ex = check_expr exp e' pr_ex in
-        begin match i with
-        | Unnamed -> raise EFunErrorTyping
-        | Named v -> 
-            begin match lookup_ref v e' with
-            | Some t -> TArrow(t, t_ex)
-            | None -> raise EFunErrorTyping
+          let t_ex = check_expr exp e' pr_ex in
+            begin match i with
+            | Unnamed -> raise EFunTypingError
+            | Named v ->
+              begin match lookup_ref v e' with
+              | Some t -> TArrow(t, t_ex)
+              | None -> raise EFunTypingError
+              end
             end
-        end
-    | Some t  ->
+    | Some t ->
         let ty'' = check_typ t e in
-        let e' = bind i ty'' e in
-        TArrow(ty'', check_expr exp e' pr_ex)
+          let e' = bind i ty'' e in
+            TArrow(ty'', check_expr exp e' pr_ex)
 
   and check_EVar v e pr_ex =
-        if identifier v 
-        then lookup v 
-        else 
-            begin match ((lookup_ref v e),pr_ex) with 
-            | (Some t,Some pr_t) -> 
-                 let t_f = unification t pr_t in
-                 define_ref v t_f e ; t_f
-            | (Some t_f , _ ) ->
-                 define_ref v t_f e ; t_f
-            | (None,Some t_f) -> 
-                 define_ref v t_f e ; t_f
-            | (None,_) -> raise (UndeclaredVariable v)
-            end
+    if identifier v
+    then lookup v
+    else begin match ((lookup_ref v e), pr_ex) with
+        | (Some t, Some pr_t) ->
+            let t_f = unification t pr_t in
+            define_ref v t_f e ; t_f
+        | (Some t_f , _ ) ->
+            define_ref v t_f e ; t_f
+        | (None, Some t_f) ->
+            define_ref v t_f e ; t_f
+        | (None, _) -> raise (UndeclaredVariable v)
+    end
 
   and check_ESum constr ty_op ex_op e =
-     match (ty_op,ex_op) with
-        | (Some ty, Some ex) ->
-            let t_ty = check_typ ty e in
-            let t_ex = check_expr ex e None in
-            TConstructor(constr , Some (unification t_ty t_ex) )
-        | (None ,Some ex) ->
-            let t_ex = check_expr ex e None in
-            TConstructor(constr , Some t_ex )
-        | (Some ty ,None ) -> TConstructor(constr, Some (check_typ ty e))
-        | (None ,None ) -> TConstructor(constr,None)
+    match (ty_op, ex_op) with
+    | (Some ty, Some ex) ->
+        let t_ty = check_typ ty e in
+        let t_ex = check_expr ex e None in
+        TConstructor(constr , Some (unification t_ty t_ex) )
+    | (None , Some ex) ->
+        let t_ex = check_expr ex e None in
+        TConstructor(constr , Some t_ex )
+    | (Some ty , None ) -> TConstructor(constr, Some (check_typ ty e))
+    | (None , None ) -> TConstructor(constr, None)
 
   and check_EProd l_pr ty_op e =
     match l_pr with
     | [] -> []
-    | (constr,ex_op)::l_pr' -> 
+    | (constr, ex_op)::l_pr' ->
         (check_ESum constr ty_op ex_op e)::(check_EProd l_pr' ty_op e)
 
   and check_branchs brs e =
     match brs with
-    | []    -> raise BranchsErrorVide
-    | Branch(p,ex)::[] -> check_branch p ex e
-    | Branch(p,ex)::brs' ->
+    | []    -> raise EmptyBranchError
+    | Branch(p, ex)::[] -> check_branch p ex e
+    | Branch(p, ex)::brs' ->
         let t_brs' = check_branchs brs' e in
-        let t_br = check_branch p ex e in
-        unification t_br t_brs'
+          let t_br = check_branch p ex e in
+            unification t_br t_brs'
 
   and check_branch p ex e = check_pattern p ex e
-    
+
   and check_pattern patt ex e =
     match patt with
-    | PSum(constr, ty_op, patt_op)   -> 
+    | PSum(constr, ty_op, patt_op) ->
         begin match check_PSum constr ty_op patt_op ex e with
         | Some p -> TArrow(TSum([p]), (check_expr ex e None))
-        | None -> TArrow(TSum([TConstructor(constr,None)]) ,(check_expr ex e None))
+        | None ->
+            TArrow(TSum([TConstructor(constr, None)]) , (check_expr ex e None))
         end
-    | PProd(ty_op, l_pr)    ->
+    | PProd(ty_op, l_pr) ->
         TArrow(TProd(check_PProd l_pr ty_op ex e ), (check_expr ex e None))
-    | PAnd(p1,p2)   -> 
+    | PAnd(p1, p2) ->
         unification (check_pattern p1 ex e) (check_pattern p1 ex e)
-    | POr(p1,p2)    -> 
+    | POr(p1, p2) ->
         unification (check_pattern p1 ex e) (check_pattern p1 ex e)
-    | PNot(p)       -> check_pattern p ex e 
-    | PVar(v)       -> 
+    | PNot(p) -> check_pattern p ex e
+    | PVar(v) ->
         let e' = bind (Named v) (typage "_alpha_"  (compt ())) e in
-        TArrow((typage "_alpha_"  (compt ())), (check_expr ex e' None))
-    | PZero         -> TArrow((typage "_alpha_"  (compt ())), (check_expr ex e None))
-    | POne          -> TArrow((typage "_alpha_"  (compt ())), (check_expr ex e None))
+          TArrow((typage "_alpha_"  (compt ())), (check_expr ex e' None))
+    | PZero ->
+        TArrow((typage "_alpha_"  (compt ())), (check_expr ex e None))
+    | POne ->
+        TArrow((typage "_alpha_"  (compt ())), (check_expr ex e None))
 
   and check_PSum constr ty_op patt_op p_ex e =
-     match (ty_op, patt_op) with
-        | (Some ty, Some patt) ->
-            let t_ty = check_typ ty e in
-            let t_patt = check_pattern patt p_ex e in
+    match (ty_op, patt_op) with
+    | (Some ty, Some patt) ->
+        let t_ty = check_typ ty e in
+          let t_patt = check_pattern patt p_ex e in
             Some(TConstructor(constr , Some (unification t_ty t_patt)))
-        | (None ,Some patt) ->
-            let t_patt = check_pattern patt p_ex e in
-            Some(TConstructor(constr , Some t_patt ))
-        | (None ,None ) -> None
-        | (_,_) -> raise PSumErrorTyping 
+    | (None , Some patt) ->
+        let t_patt = check_pattern patt p_ex e in
+          Some(TConstructor(constr , Some t_patt ))
+    | (None , None ) -> None
+    | (_, _) -> raise PSumTypingError
 
   and check_PProd l_pr ty_op p_ex e =
     match l_pr with
     | [] -> []
-    | (constr,patt_op)::l_pr' -> 
+    | (constr, patt_op)::l_pr' ->
         begin match check_PSum constr ty_op patt_op p_ex e with
         | Some p -> p::(check_PProd l_pr' ty_op p_ex e)
-        | None -> (TConstructor(constr,None))::(check_PProd l_pr' ty_op p_ex e)
+        | None   ->
+            (TConstructor(constr, None))::(check_PProd l_pr' ty_op p_ex e)
         end
 
   and check_typs tys e =
     match tys with
     | [] -> []
-    | ty::tys' -> (check_typ ty e)::(check_typs tys' e) 
+    | ty::tys' -> (check_typ ty e)::(check_typs tys' e)
 
   and check_typ_constr ty_constr e =
     match ty_constr with
     | [] -> []
-    | (TConstructor(c, Some ty))::tyc' -> 
-        (TConstructor(c, Some (check_typ ty e)))::(check_typ_constr tyc' e) 
-    | (TConstructor(_,_) as constr)::tyc' -> 
-        (constr)::(check_typ_constr tyc' e) 
+    | (TConstructor(c, Some ty))::tyc' ->
+        (TConstructor(c, Some (check_typ ty e)))::(check_typ_constr tyc' e)
+    | (TConstructor(_, _) as constr)::tyc' ->
+        (constr)::(check_typ_constr tyc' e)
 
   and check_typ ty e =
-    match ty with 
-    | TVar(t_i,tys) -> 
-        if Hashtbl.mem sign t_i 
+    match ty with
+    | TVar(t_i, tys) ->
+        if Hashtbl.mem sign t_i
         then TVar(t_i, check_typs tys e)
-        else raise TVarErrorTyping
-    | TArrow(t1,t2)     -> TArrow(check_typ t1 e, check_typ t2 e)
-    | TSum(l_constr)    -> TSum(check_typ_constr l_constr e)
-    | TProd(l_constr)   -> TProd(check_typ_constr l_constr e)
-    | TRec(t_i,tr_ty)   -> 
-        if Hashtbl.mem sign t_i 
+        else raise TVarTypingError
+    | TArrow(t1, t2)   -> TArrow(check_typ t1 e, check_typ t2 e)
+    | TSum(l_constr)   -> TSum(check_typ_constr l_constr e)
+    | TProd(l_constr)  -> TProd(check_typ_constr l_constr e)
+    | TRec(t_i, tr_ty) ->
+        if Hashtbl.mem sign t_i
         then TRec(t_i, (check_typ tr_ty e))
-        else raise TVarErrorTyping
+        else raise TVarTypingError
 
-  in 
-  if !flag then 
-     (add_op_defauld () ; check p (empty ()) )
+  in
+    if !flag then
+      (add_op_default () ; check p (empty ()) )
